4819ff6af128d7ad2285aeb73d808e730b7b872d
weiliang <303779435@qq.com>
2019-09-22 11:21:15 +0800

    引入ui组件

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,15 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+// sass 有两种语法
+// (1) scss 和less 的语法比较类似的,该嵌套还是嵌套 区别:声明变量$
+*{
+  margin: 0;
+  padding: 0;
 }
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+
 </style>
---
+++ src/components/Login.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="Login">
+    <!-- vue按钮组件 -->
+    <!--
+      type  success 绿色 danger 红色  primary 深蓝色 info 灰黑色 warning 金黄色
+      plain 简约
+      circle 圆(椭圆)border-radius:50%
+      round 圆角,胶囊
+    -->
+    <el-button type="primary">默认按钮</el-button>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
